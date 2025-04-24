5b387973b0be53d75acb23175a7d807a91eebea7
Nayem1004 <nayem@procoder.org>
2025-04-24 12:21:45 +0600

    image slider in one function

--- index.js
+++ index.js
@@ -22,3 +22,46 @@ function removeStyle2(){
     var css = document.querySelector("#pera");
     css.classList.remove("pera-style");
 }
+
+var photos = ["image/1.jpg", "image/2.jpg", "image/man-2.jpg", "image/man1.png"];
+var imgTag = document.getElementById("slide");
+var count = 0;
+
+// function next(){
+//     count++;
+//     if(count>=photos.length){
+//         count=0;
+//         imgTag.src = photos[count];
+//     }else{
+//         imgTag.src = photos[count];
+//     }
+
+// }
+
+// function prev(){
+//     count--;
+//     if(count < 0){
+//         count = photos.length -1;
+//         imgTag.src=photos[count];
+//     }else{
+//         imgTag.src = photos[count];
+//     }
+    
+// }
+
+
+
+function slider(direction) {
+    if (direction === 'next') {
+        count++;
+        if (count >= photos.length) {
+            count = 0;
+        }
+    } else if (direction === 'prev') {
+        count--;
+        if (count < 0) {
+            count = photos.length - 1;
+        }
+    }
+    imgTag.src = photos[count];
+}
